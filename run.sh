7c345d564a6a348e76ff280ef4f7f1441af6072e
welmends <wellmend0@gmail.com>
2021-01-23 14:56:42 -0300

    add data.ldif and ldapadd this entry context

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo "> Starting LDAP Server.."
+systemctl start slapd
+
+echo "> ldapadd data.ldif"
+ldapadd -x -D cn=admin,dc=example,dc=com -w secret -f /home/data.ldif
+
 while true; do
     if ! (systemctl status slapd.service | grep running)
         then
-        echo "Starting LDAP Server.."
+        echo "> Starting LDAP Server.."
         systemctl start slapd
     fi
 sleep 5
